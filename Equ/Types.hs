c9c974f86c891d179041dfaf213e4f4556ba28eb
Emmanuel <manu@manu-laptop.(none)>
2011-07-07 15:29:27 -0300

    Types instancia de la clase Data.Poset

--- Equ/Types.hs
+++ Equ/Types.hs
@@ -5,14 +5,27 @@
 
 module Equ.Types where
 import Data.Text
+import qualified Data.Poset as ModPoset
 
 data AtomTy = ATyNum
             | ATyInt
             | ATyNat
             | ATyBool -- ^ Corresponde a las fórmulas proposicionales.
+     deriving (Eq,Show)
 
 data Type = TyUnknown
           | TyVar Text
           | TyList Type
           | TyAtom AtomTy
           | Type :-> Type
+    deriving (Eq,Show)
+
+
+instance ModPoset.Poset Type where
+    (<=) (TyAtom ATyNat) (TyAtom ATyInt) = True
+    (<=) (TyAtom ATyInt) (TyAtom ATyNum) = True
+    (<=) (TyAtom ATyNat) (TyAtom ATyNum) = True
+    (<=) (TyList t1) (TyList t2) = t1 ModPoset.<= t2
+    (<=) (t1 :-> t2) (s1 :-> s2) = t1 ModPoset.<= t2 && s1 ModPoset.<= s2
+    (<=) t1 t2 = t1==t2
+
