72ad5fff86858a14c84d9e1bf665955ba55b3853
Emmanuel <manu@manu-laptop.(none)>
2011-07-28 10:53:36 -0300

    Teoria de Listas con mas operaciones

--- Equ/Theories/Arith.hs
+++ Equ/Theories/Arith.hs
@@ -1,5 +1,53 @@
 -- | La teoría de aritmética.
 module Equ.Theories.Arith where
 
+import Prelude hiding (sum)
+import Data.Text (pack)
+
+import Equ.Syntax
+import Equ.Types
+import Equ.Expr
+import Equ.PreExpr
+import Equ.Rule
+import Equ.Theories.AbsName
+
 -- Estos módulos definen los símbolos de función
 -- que devuelven expresiones de tipo Num.
+
+natZero :: Constant
+natZero = Constant { conRepr = pack "0"
+                , conName = Zero
+                , conTy = TyAtom ATyNat
+                }
+
+natSucc :: Operator
+natSucc = Operator { opRepr = pack "+1"
+                    , opName = Succ
+                    , opTy = TyAtom ATyNat :-> TyAtom ATyNat
+                    }
+                    
+                    
+natSum :: Operator
+natSum = Operator { opRepr = pack "+"
+                  , opName = Sum
+                  , opTy = TyAtom ATyNat :-> TyAtom ATyNat :-> TyAtom ATyNat
+                  }
+
+
+-- Constructor de Variable de tipo Nat.
+varNat :: String -> Expr
+varNat s = Expr $ Var $ var s (TyAtom ATyNat)
+
+-- Constructor de Constante zero
+zero :: Expr
+zero = Expr $ Con $ natZero
+
+-- Constructor de sucesor.
+-- PRE: La expresión n es del tipo adecuado
+successor :: Expr -> Expr
+successor (Expr n) = Expr $ UnOp natSucc n
+
+-- Constructor de suma
+-- PRE: Las expresiones deben ser del tipo correcto
+sum :: Expr -> Expr-> Expr
+sum (Expr n) (Expr m) = Expr $ BinOp natSum n m
--- Equ/Theories/List.hs
+++ Equ/Theories/List.hs
@@ -3,7 +3,7 @@
 
 module Equ.Theories.List where
     
-import Prelude hiding (concat)
+import Prelude hiding (concat,take,drop,length,sum)
 import Equ.Syntax
 import Equ.Types
 import Equ.Expr
@@ -12,7 +12,9 @@ import Equ.Rule
 import Equ.Theories.AbsName
 
 import Data.Text (pack)
-    
+
+import Equ.Theories.Arith (zero,successor,sum)
+
 -- | Constructor del tipo de listas polimorficas; el string indica el
 -- nombre de la variable de tipo
 tyListVar :: String -> Type
@@ -41,6 +43,25 @@ listConcat = Operator { opRepr = pack "++"
                       , opName = Concat
                       , opTy = tyListVar "A" :-> tyListVar "A" :-> tyListVar "A"
                       }
+                      
+listLength :: Operator
+listLength = Operator { opRepr = pack "#"
+                    , opName = Length
+                    , opTy = tyListVar "A" :-> TyAtom ATyNat
+                    }
+
+listTake :: Operator
+listTake = Operator { opRepr = pack "↑"
+                    , opName = Take
+                    , opTy = tyListVar "A" :-> TyAtom ATyNat :-> tyListVar "A"
+                    }
+
+listDrop :: Operator
+listDrop = Operator { opRepr = pack "↓"
+                    , opName = Drop
+                    , opTy = tyListVar "A" :-> TyAtom ATyNat :-> tyListVar "A"
+                    }
+
 
 -- | Constructor de variable de tipo lista polimorfica; el primer string es
 -- el nombre de la variable, el segundo el nombre de la variable de tipo
@@ -61,6 +82,22 @@ append (Expr x) (Expr xs) = Expr $ BinOp listApp x xs
 concat :: Expr -> Expr -> Expr
 concat (Expr xs) (Expr ys) = Expr $ BinOp listConcat xs ys
 
+-- | Constructor de length
+length :: Expr -> Expr
+length (Expr xs) = Expr $ UnOp listLength xs
+
+-- | Constructor de take
+take :: Expr -> Expr -> Expr
+take (Expr xs) (Expr n) = Expr $ BinOp listTake xs n
+
+-- | Constructor de drop
+drop :: Expr -> Expr -> Expr
+drop (Expr xs) (Expr n) = Expr $ BinOp listDrop xs n
+
+-- | Constructor de index
+index :: Expr -> Expr -> Expr
+index (Expr xs) (Expr n) = Expr $ BinOp listIndex xs n
+
 emptyConcat :: Rule
 emptyConcat = Rule { lhs = concat emptyList varYS
                    , rhs = varYS
@@ -78,3 +115,87 @@ consConcat = Rule { lhs = concat (append varX varXS) varYS
     where varX = Expr $ Var $ var "x" $ tyVar "A"
           varXS = varList "xs" "A"
           varYS = varList "ys" "A"
+
+
+-- Reglas para la definicion de length (#)
+emptyLength :: Rule
+emptyLength = Rule { lhs = length emptyList
+                   , rhs = zero
+                   , rel = relEq
+                   , desc = pack ""
+                   }
+
+consLength :: Rule
+consLength = Rule { lhs = length (append varX varXS)
+                  , rhs = successor (length varXS)
+                  , rel = relEq
+                  , desc = pack ""
+                  }
+    where varX = Expr $ Var $ var "x" $ tyVar "A"
+          varXS = varList "xs" "A"
+
+-- Reglas para la definicion de take
+zeroTake :: Rule
+zeroTake = Rule { lhs = take varXS zero
+                , rhs = emptyList
+                , rel = relEq
+                , desc = pack ""
+                }
+    where varXS = varList "xs" "A"
+                      
+                
+emptyTake :: Rule
+emptyTake = Rule { lhs = take emptyList varN
+                 , rhs = emptyList
+                 , rel = relEq
+                 , desc = pack ""
+                 }
+    where varN = Expr $ Var $ var "x" $ TyAtom ATyNat
+
+consTake :: Rule
+consTake = Rule { lhs = take (append varX varXS) (successor varN)
+                , rhs = append varX $ take varXS varN
+                , rel = relEq
+                , desc = pack ""
+                }
+    where varXS = varList "xs" "A"
+          varX = Expr $ Var $ var "x" $ tyVar "A"
+          varN = Expr $ Var $ var "x" $ TyAtom ATyNat
+          
+
+-- Reglas para la definicion de drop
+zeroDrop :: Rule
+zeroDrop = Rule { lhs = drop varXS zero
+                , rhs = varXS
+                , rel = relEq
+                , desc = pack ""
+                }
+    where varXS = varList "xs" "A"
+                
+emptyDrop :: Rule
+emptyDrop = Rule { lhs = drop emptyList varN
+                 , rhs = emptyList
+                 , rel = relEq
+                 , desc = pack ""
+                 }
+    where varN = Expr $ Var $ var "x" $ TyAtom ATyNat
+
+consDrop :: Rule
+consDrop = Rule { lhs = drop (append varX varXS) (successor varN)
+                , rhs = drop varXS varN
+                , rel = relEq
+                , desc = pack ""
+                }
+    where varXS = varList "xs" "A"
+          varX = Expr $ Var $ var "x" $ tyVar "A"
+          varN = Expr $ Var $ var "x" $ TyAtom ATyNat
+          
+-- Reglas para la definicion de Index
+zeroIndex :: Rule
+zeroIndex = Rule { lhs = index (append varX varXS) zero
+                 , rhs = varX
+                 , rel = relEq
+                 , desc = pack ""
+                 }
+    where varXS = varList "xs" "A"
+          varX = Expr $ Var $ var "x" $ tyVar "A"
