83e1b1331604809465c8fa201497961be932a500
Miguel Pagano <miguel.pagano@gmail.com>
2011-08-12 18:11:12 -0300

    Split zipper from PreExpr.hs; split parser of types; idiomacies in parsers.

---
+++ Equ/Parser/Types.hs
@@ -0,0 +1,116 @@
+-- | Este modulo es el parser de expresiones de tipos. 
+
+module Equ.Parser.Types (parseTy) where
+
+import Text.Parsec
+import Text.Parsec.Token
+import Text.Parsec.Language
+import Text.Parsec.String
+import qualified Text.Parsec.Expr as PE
+import Data.Text (pack,unpack)
+
+import Control.Monad.Identity
+import Control.Applicative ((<$>),(<$))
+import Equ.Types
+
+
+
+-- | Para definir la función anterior podemos necesitar definir 
+-- esta función para poder parsear los tipos que el usuario quiera
+-- asignar a los diferentes constituyentes de pre-expresiones.
+-- parserTy :: Text -> Either ParseError Type
+-- parserTy = undefined
+
+type ParserTypeTable = PE.OperatorTable String () Identity Type
+
+-- Numeros
+num :: String
+num = "Num"
+
+-- Enteros
+int :: String
+int = "Int"
+
+-- Naturales
+nat :: String
+nat = "Nat"
+
+-- Booleanos.
+bool :: String
+bool = "Bool"
+
+-- Operador de funcion.
+opFun :: String
+opFun = ":->"
+
+-- | Nombre de los tipos atomicos.
+listAtomTy :: [String]
+listAtomTy = [num, int, nat, bool]
+
+-- | Nombre para indicar tipo desconocido.
+typeUnknown :: String
+typeUnknown = "¿Type?"
+
+lexerTy :: TokenParser ()
+lexerTy = makeTokenParser $
+            emptyDef { reservedOpNames = [opFun]
+                     , reservedNames = typeUnknown : listAtomTy
+                     , identStart  = letter <|> char '_'
+                     , identLetter = alphaNum <|> char '_'
+                     }
+
+operatorTypeTable :: ParserTypeTable
+operatorTypeTable = [[PE.Infix (reservedOp lexerTy opFun >> return (:->)) PE.AssocRight]]
+
+typeSubExpr :: Parser Type
+typeSubExpr =  parseUnknown
+           <|> parseTyVar
+           <|> parseTyList
+           <|> parseTyAtom
+
+-- Parseo de tipos.
+parseType :: Parser Type
+parseType = PE.buildExpressionParser operatorTypeTable typeSubExpr 
+               <?> "Parser error: Expresión mal formada"
+
+-- | Parser para los tipos atomicos.
+parseAtomTy :: Parser AtomTy
+parseAtomTy = foldr ((<|>) . uncurry patom) parserZero atoms 
+    where atoms = [(num,ATyNum),(int,ATyInt),(nat,ATyNat),(bool,ATyBool)]
+          patom iden ty = ty <$ (try $ reserved lexerTy iden)
+
+-- | Parser para el tipo Unknown.
+parseUnknown :: Parser Type
+parseUnknown = TyUnknown <$ (try $ reserved lexerTy typeUnknown)
+
+-- | Parser para el tipo variable.
+parseTyVar :: Parser Type
+parseTyVar = try $ lower >>= \l -> many letter >>= \t -> return (tyVar (l:t))
+
+-- | Parser para el tipo lista.
+parseTyList :: Parser Type
+parseTyList = TyList <$> brackets lexerTy parseType
+
+-- | Parser para el tipo de tipos atomicos.
+parseTyAtom :: Parser Type
+parseTyAtom = TyAtom <$> try parseAtomTy
+
+-- | Funcion principal de parseo desde string.
+parseTyFromString :: String -> Either ParseError Type
+parseTyFromString = parse parseType ""
+
+-- | Parser de tipos.
+parseTy :: String -> Type
+parseTy = either showError showType . parseTyFromString
+
+-- | Imprimimos Types usando que es instancia de show.
+showType :: a -> a
+showType = id
+
+-- Expresiones de prueba:
+-- (F@(succ 0) + x) ▹ [] ⇒ True
+-- 〈∃ x : (G@(# []) + x) ▹ [] ⇒ True : p ⇒ q〉
+
+-- Imprimimos el error con version Exception de haskell.
+showError :: Show a => a -> b
+showError = error . show
---
+++ Equ/PreExpr/Internal.hs
@@ -0,0 +1,105 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+module Equ.PreExpr.Internal where
+
+import Equ.Syntax
+import Data.Monoid
+import Data.Foldable (Foldable(..))
+import qualified Data.Foldable as F
+import Control.Applicative ((<$>), (<*>),Applicative(..))
+import Test.QuickCheck(Arbitrary, arbitrary, oneof)
+
+{- Propiedades de PreExpresiones (QC): queremos poder respetar la forma
+   de escribir una expresión.
+   
+    * 'pretty . parser = id'
+    * 'parser . pretty = id'
+-}
+
+data PreExpr' a = Var a
+                | Con Constant
+                | Fun Func
+                | PrExHole Hole
+                | UnOp Operator (PreExpr' a)
+                | BinOp Operator (PreExpr' a) (PreExpr' a)
+                | App (PreExpr' a) (PreExpr' a)
+                | Quant Quantifier a (PreExpr' a) (PreExpr' a)
+                | Paren (PreExpr' a)
+                  deriving Eq
+
+type PreExpr = PreExpr' Variable
+
+instance Functor PreExpr' where
+    fmap f (Var a) = Var $ f a
+    fmap _ (Con c) = Con c
+    fmap _ (Fun g) = Fun g
+    fmap _ (PrExHole h) = PrExHole h
+    fmap f (UnOp op e) = UnOp op $ fmap f e
+    fmap f (BinOp op e e') = BinOp op (fmap f e) (fmap f e')
+    fmap f (App e e') = App (fmap f e) (fmap f e')
+    fmap f (Quant q a e e') = Quant q (f a) (fmap f e) (fmap f e')
+    fmap f (Paren e) = Paren $ fmap f e
+
+instance Foldable PreExpr' where
+    foldMap f (Var v) = f v
+    foldMap f (UnOp _ e) = foldMap f e
+    foldMap f (BinOp _ e e') = foldMap f e `mappend` foldMap f e'
+    foldMap f (App e e') = foldMap f e `mappend` foldMap f e'
+    foldMap f (Quant q v e e') = f v `mappend` foldMap f' e `mappend` foldMap f' e'
+        where f' w = f w 
+--                   | v == w = mempty
+--                   | otherwise = f w
+    foldMap _ (PrExHole _) = mempty
+    foldMap f (Paren e) = foldMap f e
+
+{-
+instance Applicative PreExpr' where
+    pure = Var
+    Var f <*> Var v = Var $ f v
+    f <*> UnOp op e = UnOp op $ f <*> e
+    f <*> BinOp op e e' = BinOp op (f <*> e) (f <*> e')
+    f <*> App e e' = App (f <*> e) (f <*> e')
+    f <*> Quant q v e e' = Quant 
+    _ <*> (Con c) = Con c
+    _ <*> (Fun g) = Fun g
+    _ <*> (PrExHole h) = PrExHole h
+{-    
+    fmap f (BinOp op e e') = BinOp op (fmap f e) (fmap f e')
+    fmap f (App e e') = App (fmap f e) (fmap f e')
+    fmap f (Quant q a e e') = Quant q (f a) (fmap f e) (fmap f e')
+    fmap f (Paren e) = Paren $ fmap f e
+-}
+-}
+
+
+
+-- | Instancia arbitrary para las preExpresiones, lo único que dejamos fijo es el 
+-- operador unario, esto para simplificar la forma de las preExpresiones.
+instance Arbitrary PreExpr where
+    arbitrary =
+        oneof [   Var <$> arbitrary
+                , Con <$> arbitrary
+                , Fun <$> arbitrary
+                , PrExHole <$> arbitrary
+                , UnOp <$> arbitrary <*> arbitrary
+                , BinOp <$> arbitrary <*> arbitrary <*> arbitrary
+                , App <$> arbitrary <*> arbitrary
+                , Quant <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+                , Paren <$> arbitrary
+                ]
+
+
+
+-- | Pretty print para las preExpresiones.
+instance Show PreExpr where
+    show (Var x) = show x
+    show (Con k) = show k
+    show (Fun f) = show f
+    show (PrExHole h) = show h
+    show (UnOp op preExp) = show op ++ "(" ++ show preExp ++ ")"
+    show (BinOp op preExp0 preExp1) = "(" ++ show preExp0 ++ ")" ++ show op ++ 
+                                      "(" ++ show preExp1 ++ ")"
+    show (App preExp0 preExp1) = show preExp0 ++ " " ++ "(" ++ show preExp1 ++ ")"
+    show (Quant qua v preExp0 preExp1) = show qua ++ show v ++ " in " 
+                                        ++ show preExp0 ++ " | " 
+                                        ++ show preExp1
+    show (Paren e) = "〔" ++ show e ++ " 〕"
